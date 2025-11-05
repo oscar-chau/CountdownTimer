b977a7b4ba56d9c96be5e9bf9c7a14de38408104
Oscar Chau <oscarchau126@gmail.com>
2025-11-05 09:31:46 -0800

    cancel timer

--- app/src/main/java/com/zybooks/countdowntimer/ui/TimerViewModel.kt
+++ app/src/main/java/com/zybooks/countdowntimer/ui/TimerViewModel.kt
@@ -62,5 +62,14 @@ class TimerViewModel : ViewModel() {
 
    fun cancelTimer() {
       // TODO: Cancel the timer
+      if (isRunning) {
+         timerJob?.cancel()
+         isRunning = false
+         remainingMillis = 0
+      }
+   }
+   override fun onCleared() {
+      super.onCleared()
+      timerJob?.cancel()
    }
 }
